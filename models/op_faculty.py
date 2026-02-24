fd52c5b02a577599e21c0b495fb4e1defb6c1c7f
jihadazam <j.azam@upm.ac.ma>
2026-02-24 12:30:14 +0000

    FIXED

--- models/op_faculty.py
+++ models/op_faculty.py
@@ -15,8 +15,9 @@ class OpFaculty(models.Model):
     contract_ids = fields.One2many('faculty.contract', 'faculty_id', string='Contrats')
     assignment_ids = fields.One2many('faculty.assignment', 'faculty_id', string='Affectations')
 
+
     @api.model
     def create(self, vals):
         if vals.get('faculty_code', _('New')) == _('New'):
             vals['faculty_code'] = self.env['ir.sequence'].next_by_code('op.faculty') or _('New')
-        return super().create(vals)
+        return super(OpFaculty, self).create(vals)
